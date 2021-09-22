1d5713868bc4ddd4475f3efe519fc16068241860
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-09-22 18:51:12 -0300

    Adicionar arquivos de projeto.

---
+++ src/AppMetricsForce/Models/Meta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppMetricsForce.Models
+{
+    public class Meta
+    {
+        public int Id { get; set; }
+        public DateTime InicioMeta { get; set; }
+        public DateTime FimMeta { get; set; }
+        public double RealizadoAteDataAtual { get; set; }
+        public double MetaBronze { get; set; }
+        public double MetaPrata { get; set; }
+        public double MetaOuro { get; set; }
+        public double MetaDiamante { get; set; }
+
+    }
+}
