9222c40328f9381ace0cc465854e0a6d8b948643
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-09-22 19:40:21 -0300

    Adiciona controllers

---
+++ src/AppMetricsForce/Controllers/VendedoresController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AppMetricsForce.App.Data.Context;
+using AppMetricsForce.Models;
+
+namespace AppMetricsForce.Controllers
+{
+    public class VendedoresController : Controller
+    {
+        private readonly MetricsDbContext _context;
+
+        public VendedoresController(MetricsDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Vendedors
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.vendedores.ToListAsync());
+        }
+
+        // GET: Vendedors/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vendedor = await _context.vendedores
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vendedor == null)
+            {
+                return NotFound();
+            }
+
+            return View(vendedor);
+        }
+
+        // GET: Vendedors/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Vendedors/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,Login,Senha,CodigoFuncionario,Cargo,Ativo")] Vendedor vendedor)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(vendedor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vendedor);
+        }
+
+        // GET: Vendedors/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vendedor = await _context.vendedores.FindAsync(id);
+            if (vendedor == null)
+            {
+                return NotFound();
+            }
+            return View(vendedor);
+        }
+
+        // POST: Vendedors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Login,Senha,CodigoFuncionario,Cargo,Ativo")] Vendedor vendedor)
+        {
+            if (id != vendedor.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(vendedor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VendedorExists(vendedor.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vendedor);
+        }
+
+        // GET: Vendedors/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vendedor = await _context.vendedores
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vendedor == null)
+            {
+                return NotFound();
+            }
+
+            return View(vendedor);
+        }
+
+        // POST: Vendedors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var vendedor = await _context.vendedores.FindAsync(id);
+            _context.vendedores.Remove(vendedor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VendedorExists(int id)
+        {
+            return _context.vendedores.Any(e => e.Id == id);
+        }
+    }
+}
