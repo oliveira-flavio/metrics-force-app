1ca01ea48c11fcde2b90b461ab1b72e4d1edb39a
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-09-22 19:40:37 -0300

    Adiciona propriedade no usuario

--- src/AppMetricsForce/Models/Usuario.cs
+++ src/AppMetricsForce/Models/Usuario.cs
@@ -7,6 +7,7 @@ namespace AppMetricsForce.Models
 {
     public class Usuario
     {
+        
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Login { get; set; }
