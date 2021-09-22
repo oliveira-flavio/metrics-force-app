d66f03b207b762df4b808d0b0f9fb2fd4427614b
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-09-22 19:40:05 -0300

    Adiciona DbContext

---
+++ src/AppMetricsForce/App.Data/Context/MetricsDbContext.cs
@@ -0,0 +1,21 @@
+﻿using AppMetricsForce.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppMetricsForce.App.Data.Context
+{
+    public class MetricsDbContext : DbContext
+    {
+        public MetricsDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Administrador> administradores { get; set; }
+        public DbSet<Gerente> gerentes { get; set; }
+        public DbSet<Vendedor> vendedores { get; set; }
+        public DbSet<Meta> metas { get; set; }
+        public DbSet<Usuario> usuarios { get; set; }
+    }
+}
