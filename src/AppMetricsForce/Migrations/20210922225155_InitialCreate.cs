2b1e8d60be490de66deaf8c779535f6605fa5dc1
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-09-22 21:24:34 -0300

    Adiciona migrações

---
+++ src/AppMetricsForce/Migrations/20210922225155_InitialCreate.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace AppMetricsForce.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "metas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    InicioMeta = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    FimMeta = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    RealizadoAteDataAtual = table.Column<double>(type: "double precision", nullable: false),
+                    MetaBronze = table.Column<double>(type: "double precision", nullable: false),
+                    MetaPrata = table.Column<double>(type: "double precision", nullable: false),
+                    MetaOuro = table.Column<double>(type: "double precision", nullable: false),
+                    MetaDiamante = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_metas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "text", nullable: true),
+                    Login = table.Column<string>(type: "text", nullable: true),
+                    Senha = table.Column<string>(type: "text", nullable: true),
+                    CodigoFuncionario = table.Column<string>(type: "text", nullable: true),
+                    Cargo = table.Column<int>(type: "integer", nullable: false),
+                    Ativo = table.Column<bool>(type: "boolean", nullable: false),
+                    Discriminator = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_usuarios", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "metas");
+
+            migrationBuilder.DropTable(
+                name: "usuarios");
+        }
+    }
+}
