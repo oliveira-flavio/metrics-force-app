675d740eb37ac344222b95303c25a99f4e5fa2bd
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-09-22 21:25:28 -0300

    Adiciona propriedades

--- src/AppMetricsForce/Models/Vendedor.cs
+++ src/AppMetricsForce/Models/Vendedor.cs
@@ -7,5 +7,16 @@ namespace AppMetricsForce.Models
 {
     public class Vendedor : Usuario
     {
+        public int QuantidadeDeVendas { get; set; }
+        public double VendaLiquida { get; set; }
+        public int ItensVendidos { get; set; }
+        public double TicketMedio { get; set; }
+
+        //Comissão
+        //Bonus por atingimento de meta diamante.
+        //Ao cadastrar meta, gerente pode informar se a meta diamante possui bonus por atingimento
+        //Existindo bonus, informar o valor do bonus pelo atingimento
+        //No dashboard do vendedor, mostrar quanto falta para ele conquistar a meta
+
     }
 }
