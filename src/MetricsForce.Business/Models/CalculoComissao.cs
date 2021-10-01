f9c0b1aee3a86d5768949bf28aa0713939d97bc7
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-10-01 19:11:52 -0300

    Adiciona classe de calculo de comissão

---
+++ src/MetricsForce.Business/Models/CalculoComissao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetricsForce.Business.Models
+{
+    class CalculoComissao
+    {
+        public decimal ValorRealizado { get; set; }
+        public decimal ValorBronze { get; set; }
+        public decimal ValorPrata { get; set; }
+        public decimal ValorOuro { get; set; }
+        public decimal Diamante { get; set; }
+        public float PorcentagemComissaoNormal { get; set; }
+        public float PorcentagemComissaoBronze { get; set; }
+        public float PorcentagemComissaoPrata { get; set; }
+        public float PorcentagemComissaoOuro { get; set; }
+        public float PorcentagemComissaoDiamante { get; set; }
+    }
+}
