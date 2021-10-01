0f66790877b0ac7965da6da5a76ef943b6e311df
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-10-01 16:00:57 -0300

    Adiciona context pronto na camada Data

---
+++ src/MetricsForce.Data/Context/MetricsDbContext.cs
@@ -0,0 +1,32 @@
+﻿using AppMetricsForce.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppMetricsForce.App.Data.Context
+{
+    public class MetricsDbContext : DbContext
+    {
+        public MetricsDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Administrador> Administradores { get; set; }
+        public DbSet<Gerente> Gerentes { get; set; }
+        public DbSet<Vendedor> Vendedores { get; set; }
+        public DbSet<Meta> Metas { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MetricsDbContext).Assembly);
+
+            // Impedir delete cascade
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) 
+                relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
