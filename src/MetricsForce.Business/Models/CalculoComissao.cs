e19e4e2a7fa8ba7805539eab34eb2da3622decb1
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-10-01 19:53:33 -0300

    Remove atributos desnecessários

--- src/MetricsForce.Business/Models/CalculoComissao.cs
+++ src/MetricsForce.Business/Models/CalculoComissao.cs
@@ -8,12 +8,7 @@ namespace MetricsForce.Business.Models
 {
     class CalculoComissao
     {
-        public decimal ValorRealizado { get; set; }
-        public decimal ValorBronze { get; set; }
-        public decimal ValorPrata { get; set; }
-        public decimal ValorOuro { get; set; }
-        public decimal Diamante { get; set; }
-        public float PorcentagemComissaoNormal { get; set; }
+        public float PorcentagemComissaoBase { get; set; }
         public float PorcentagemComissaoBronze { get; set; }
         public float PorcentagemComissaoPrata { get; set; }
         public float PorcentagemComissaoOuro { get; set; }
