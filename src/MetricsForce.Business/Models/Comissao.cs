20b4b49c9e43fcfe3d14bb288464ef5562fcb779
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-10-05 16:16:58 -0300

    Adiciona classe comissão

---
+++ src/MetricsForce.Business/Models/Comissao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetricsForce.Business.Models
+{
+    public class Comissao
+    {
+        public int IdComissao { get; set; }
+        public int IdUsuario { get; set; }
+        public PercentualComissao PercentualComissao { get; set; }
+    }
+}
