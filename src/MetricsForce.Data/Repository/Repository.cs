d9154a98015b25778528b4e76f2616ddbf7af947
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-10-06 14:42:41 -0300

    Adiciona repositóro padrão

---
+++ src/MetricsForce.Data/Repository/Repository.cs
@@ -0,0 +1,49 @@
+﻿using MetricsForce.Business.Interfaces;
+using MetricsForce.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetricsForce.Data.Repository
+{
+    public abstract class Repository<TEntity> : IRepository<TEntity> where TEntity : Entity
+    {
+        public Task Adicionar(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Atualizar(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> Buscar(Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> ObterPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TEntity>> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
