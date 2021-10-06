855928ae9ea70f90404bf5ee13f51a8766c2e2da
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-10-05 21:46:28 -0300

    Adiciona mapping da classe indicador de performance

---
+++ src/MetricsForce.Data/Mapping/IndicadorDePerformanceMapping.cs
@@ -0,0 +1,33 @@
+﻿using MetricsForce.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetricsForce.Data.Mapping
+{
+    public class IndicadorDePerformanceMapping : IEntityTypeConfiguration<IndicadorDePerformance>
+    {
+        public void Configure(EntityTypeBuilder<IndicadorDePerformance> builder)
+        {
+            builder.HasKey(p => p.IdIndicadorDePerformance);
+            builder.Property(p => p.TicketMedio)
+                .HasColumnType("decimal(5,2)")
+                .HasColumnName("TicketMedio")
+                .IsRequired();
+
+            builder.Property(p => p.QuantidadeVenda)
+                .HasColumnType("int2")
+                .HasColumnName("QuantidadeVenda")
+                .IsRequired();
+
+            builder.Property(p => p.Data)
+                .HasColumnName("Data");
+
+            builder.ToTable("IndicadoresDePerformance");
+        }
+    }
+}
