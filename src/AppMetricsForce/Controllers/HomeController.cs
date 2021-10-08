81f38ec3c216aac4dec05f4f43134d7ebcab8fc6
Christine Schutz <christine.schutz@outlook.com>
2021-10-07 21:35:09 -0300

    Adiciona cards no dashboard

--- src/AppMetricsForce/Controllers/HomeController.cs
+++ src/AppMetricsForce/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace AppMetricsForce.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Sobre()
         {
             return View();
         }
