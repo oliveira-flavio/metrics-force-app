90746900bb11457bea9bbc81c59880c0d505c557
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-10-19 08:57:58 -0300

    Adiciona taghelper

---
+++ src/MetricsForceApp/Extensions/ApagaElementoByClaimTagHelper.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsForceApp.Extensions
+{
+    [HtmlTargetElement("*", Attributes = "suppres-by-claim-name")]
+    [HtmlTargetElement("*", Attributes = "suppres-by-claim-value")]
+    public class ApagaElementoByClaimTagHelper : TagHelper
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public ApagaElementoByClaimTagHelper(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
+        [HtmlAttributeName("suppres-by-claim-name")]
+        public string IdentityClaimName { get; set; }
+        [HtmlAttributeName("suppress-by-claim-value")]
+        public string IdentityClaimValue { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            if (output == null)
+                throw new ArgumentNullException(nameof(output));
+
+            var temAcesso = CustomAuthorization.ValidarClaimUsuario(_contextAccessor.HttpContext, IdentityClaimName, IdentityClaimValue);
+
+            if (temAcesso) return;
+
+            output.SuppressOutput();
+        }
+    }
+}
